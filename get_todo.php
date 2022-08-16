5688f7bd2877fb44ff8b6470ce787ee03e6e7d09
prasiddha22 <prasiddhaneupane22@gmail.com>
2022-08-16 08:51:50 +0545

    Comment Added

--- get_todo.php
+++ get_todo.php
@@ -7,7 +7,7 @@ $sql = "SELECT * FROM todo";
 
 // Execute the query
 $result = mysqli_query($connect, $sql);
-
+// Create a variable array to store the retrieved data
 $todo_arr = [];
 
 while ($row[] = mysqli_fetch_assoc($result)) {
